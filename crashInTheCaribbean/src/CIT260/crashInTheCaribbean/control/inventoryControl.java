4fcb1e43dd6af8f461a25ef893ac6b6208ea4b37
thomas <thomas@EvonFamilyPC>
2016-05-28 20:27:36 -0700

    5/26/16 added in tests for trap volume and ship control

---
+++ crashInTheCaribbean/src/CIT260/crashInTheCaribbean/control/inventoryControl.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CIT260.crashInTheCaribbean.control;
+
+// author thomas evon
+
+import static java.lang.Math.PI;
+import static javax.swing.Spring.height;
+
+public class inventoryControl {
+  
+public double volumeOfTrap(bucketRadius, bucketHeight, PI) {
+    int bucketRadius = 0;
+
+if (bucketRadius <0 || bucketRadius >5) THEN {
+return -1}
+
+if (height <0 || height > 12) THEN
+return -1;
+    double bucketHeight;
+
+totalVolume = PI * (bucketRadius * bucketRadius) * bucketHeight;
+
+}
