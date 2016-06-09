4a9901133600452677be018bc86d02c93050e128
josecovarrubias <josecovarrubias@joses-imac.domain>
2016-06-08 23:20:38 -0700

    June 8, 2nd assignment working Jose.

--- crashInTheCaribbean/src/CIT260/crashInTheCaribbean/view/DamageControlView.java
+++ crashInTheCaribbean/src/CIT260/crashInTheCaribbean/view/DamageControlView.java
@@ -5,6 +5,7 @@
  */
 package CIT260.crashInTheCaribbean.view;
 
+import CIT260.crashInTheCaribbean.control.DamageControl;
 import java.util.Scanner;
 
 /**
@@ -16,34 +17,45 @@ public class DamageControlView {
     private String message;
 
     public DamageControlView(){
-        this.message ="If you reach or pass 1,024 on the power level"
-                + "you will defeat the beast attacking you, but you"
-                + "can only enter numbers from 1 through 5 in different"
-                + "combinations to reach the beast resistanse number. ";
+        this.message ="\nIf you reach or pass 1,024 on the power level"
+                + "\nyou will defeat the beast attacking you, but you"
+                + "\ncan only enter numbers from 1 through 5 in different"
+                + "\ncombinations to reach the beast resistanse number. ";
     }
     public void displayDamageControlView() {
-           System.out.println("the display funtion");
-    }
-    public void getDamageValues(){
-//           System.out.println("get values function");
-           Scanner keyboard = new Scanner(System.in); // get infile for keyboard
-       double value = 0; //value to be return
-       boolean valid = false; //initialize to not valid
-       
-       while (!valid){ //loop while an invalid value is enter
-           System.out.println("\n" + this.message);
+           System.out.println(message);
+           System.out.println("\nEnter a value for the level of attack power");
+           Scanner inFile = new Scanner(System.in);
+           int input1 = inFile.nextInt();
+           System.out.println("\nNow enter a number to rais the first"
+                            + "\nvalue to that power");
+           int input2 = inFile.nextInt();
            
-           double value = keyboard.nextLine(); //get next line typed on keyboard
+           DamageControl damage = new DamageControl();
+           double damageLevel = damage.damage(input1, input2);
+           System.out.println("Your attack was "+ damageLevel);
            
-           if (value <1 || >5){// value is blank
-               System.out.println("\nStay within the range of 1-5");
-               continue;
-           }
-       
-       }
-       return ; // return the value entered
     }
-    public void doAction(){
-            System.out.println("do action function");
-    }
+//    public void getDamageValues(){
+////           System.out.println("get values function");
+//           Scanner keyboard = new Scanner(System.in); // get infile for keyboard
+//       double value = 0; //value to be return
+//       boolean valid = false; //initialize to not valid
+//       
+//       while (!valid){ //loop while an invalid value is enter
+//           System.out.println("\n" + this.message);
+//           
+//           double value = keyboard.nextLine(); //get next line typed on keyboard
+//           
+//           if (value <1 || >5){// value is blank
+//               System.out.println("\nStay within the range of 1-5");
+//               continue;
+//           }
+//       
+//       }
+//       return ; // return the value entered
+//    }
+//    public void doAction(){
+//            System.out.println("do action function");
+//    }
 }
