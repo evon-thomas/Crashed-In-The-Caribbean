4165149d6beb044a953e82b7cd8340eb52109038
Karla <Karla@192.168.1.65>
2016-06-24 01:01:42 -0500

    06/24/2016 location emun

--- crashInTheCaribbean/src/CIT260/crashInTheCaribbean/model/Location.java
+++ crashInTheCaribbean/src/CIT260/crashInTheCaribbean/model/Location.java
@@ -29,7 +29,7 @@ public enum Location implements Serializable{
       private final boolean visited;
       private final double amountRemaning;
 //
-      Location() {
+      Location() { 
       }
         
     
